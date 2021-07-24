920f74ce68daddda13e00d616ad83274d0a24208
MrMohebi <MMMohebi@outlook.com>
2021-07-24 09:16:06 +0430

    add assets and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {BrowserRouter,Route} from "react-router-dom";
 
+//css
+import "./assets/cssReset/cssReset_v1.css"
+import "./assets/fonts/fonts.css"
+import './assets/bootstrap/css/bootstrap.css'
+
+
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <>Hi :)</>
+    <>
+      <BrowserRouter>
+        <Route exact path='/' component={Navbar}/>
+      </BrowserRouter>
+    </>
   );
 }
 
