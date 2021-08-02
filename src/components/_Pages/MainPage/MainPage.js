44429fa02383ecd3cba3b141ecf78e111d653483
MrMohebi <MMMohebi@outlook.com>
2021-08-02 09:10:22 +0430

    add Advertisement section

--- src/components/_Pages/MainPage/MainPage.js
+++ src/components/_Pages/MainPage/MainPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const MainPage = () => {
-    return (
-        <div>
+import Advertisement from "../../Advertisement/Advertisement";
 
+import "./style.css"
+
+const MainPage = () => {
+    let dummyAdd = [{id:"1",title:"انتخاب واحد",price:20, time:1627803006},{id:"3",title:"درخواست مهمان",price:50, time:16278020010}]
+    return (
+        <div className={"main-page-container"}>
+            <Advertisement adds={dummyAdd}/>
         </div>
     );
 };
