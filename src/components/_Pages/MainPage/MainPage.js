14c5ca0cfd5c9c992aa565aa7ae2548da50a32f2
MrMohebi <MMMohebi@outlook.com>
2021-09-30 22:54:03 +0330

    trend subjects

--- src/components/_Pages/MainPage/MainPage.js
+++ src/components/_Pages/MainPage/MainPage.js
@@ -17,285 +17,224 @@ import HeaderMain from "../../HeaderMain/HeaderMain";
 import "./style.css"
 
 const MainPage = () => {
-        return (
+
+    let trendSubjects = [{text:"فیزیک"},{text:"حل تمرین"},{text:"انصراف"},{text:"ریاضی دو"},{text:"پیاز"},{text:"سیب زمینی"},{text:"پزتقال"},{text:"هلو"}]
+
+
+    return (
+        <div>
             <div>
-                <div>
-                    <HeaderMain/>
-                    <SearchBar/>
+                <HeaderMain/>
+                <SearchBar/>
+            </div>
+
+            <div style={{background: '#F6F8FA'}}>
+
+                <div className={"trend-subjects"}>
+                    <h6 className={"IranSansBold"}>
+                    موضوعات پر طرفدار
+                    </h6>
+
+                    <div className={"d-flex flex-row-reverse"} style={{overflowX: 'scroll'}}>
+                        {trendSubjects.map(eS=>{
+                            return(
+                                <button className={"subject-btn"}>
+                                    {eS.text}
+                                </button>
+                            )
+                        })}
+                    </div>
                 </div>
 
-                <div style={{background: '#F6F8FA'}}>
+                <div className={"adsCards"}
+                      style={{background: '#fff', borderRadius: 15, width: '91%', margin: 15, fontFamily: IranSANS}}
+                >
+                    <img src={noteBook} alt={'note book icon'}
+                         style={{paddingTop: 16, paddingLeft: 16}}
+                    />
 
-                    <span
-                        className={"popularSubjects"}
-                    >
-
-                        <p
-                        className={"text-end"}
-                        style={{color: '#1DA1F2', fontSize: 12, padding: 10, margin: 0, fontFamily: IranSANS}}
-                        >
-                        موضوعات پر طرفدار
-                        </p>
-
-                        <div className={"d-flex flex-row-reverse"}
-                             style={{overflow: 'auto'}}
-                        >
-
-                        <button
-                                className={"btn align-item-right m-2 border"}
-                                style={{background: '#fff', border: '#E1E8ED', borderRadius: 10, fontFamily: IranSANS}}
-                            >
-                                Test
-                            </button>
-
-                            <button
-                                className={"btn align-item-right m-2 border"}
-                                style={{background: '#fff', border: '#E1E8ED', borderRadius: 10, fontFamily: IranSANS}}
-                            >
-                                Test
-                            </button>
-
-                            <button
-                                className={"btn align-item-right m-2 border"}
-                                style={{background: '#fff', border: '#E1E8ED', borderRadius: 10, fontFamily: IranSANS}}
-                            >
-                                Test
-                            </button>
-
-                            <button
-                                className={"btn align-item-right m-2 border"}
-                                style={{background: '#fff', border: '#E1E8ED', borderRadius: 10, fontFamily: IranSANS}}
-                            >
-                                Test
-                            </button>
-
-                            <button
-                                className={"btn align-item-right m-2 border"}
-                                style={{background: '#fff', border: '#E1E8ED', borderRadius: 10, fontFamily: IranSANS}}
-                            >
-                                Test
-                            </button>
-
-                            <button
-                                className={"btn align-item-right m-2 border"}
-                                style={{background: '#fff', border: '#E1E8ED', borderRadius: 10, fontFamily: IranSANS}}
-                            >
-                                Test
-                            </button>
-
-                            <button
-                                className={"btn align-item-right m-2 border"}
-                                style={{background: '#fff', border: '#E1E8ED', borderRadius: 10, fontFamily: IranSANS}}
-                            >
-                                Test
-                            </button>
-
-                            <button
-                                className={"btn align-item-right m-2 border"}
-                                style={{background: '#fff', border: '#E1E8ED', borderRadius: 10, fontFamily: IranSANS}}
-                            >
-                                Test
-                            </button>
-
-                            <button
-                                className={"btn align-item-right m-2 border"}
-                                style={{background: '#fff', border: '#E1E8ED', borderRadius: 10, fontFamily: IranSANS}}
-                            >
-                                Test
-                            </button>
-
-                        </div>
-                </span>
-
-                    <div className={"adsCards"}
-                          style={{background: '#fff', borderRadius: 15, width: '91%', margin: 15, fontFamily: IranSANS}}
-                    >
-                        <img src={noteBook} alt={'note book icon'}
-                             style={{paddingTop: 16, paddingLeft: 16}}
-                        />
-
-                        <h6 className={"text-end"}
-                            style={{fontSize: 16,
-                                    paddingRight: 16,
-                                    marginBottom: 15,
-                                    fontFamily: IranSANS
-                            }}
-                        >
-                            عنوان آگهی
-                        </h6>
-
-                        <p className={"text-end"}
-                            style={{fontSize: 10,
-                                    paddingRight: 16,
-                                    marginBottom: 0,
-                                    fontFamily: IranSANS
-                            }}
-                        >
-                            موضوع آگهی
-                        </p>
-
-                        <p
-                            className={"text-end"}
-                            style={{
-                                fontSize: 14,
+                    <h6 className={"text-end"}
+                        style={{fontSize: 16,
                                 paddingRight: 16,
-                                fontFamily: IranSANS,
-                                marginTop: 15,
-                                paddingBottom: 17
-                            }}
-                        >
-                            بدون محدودیت
-                        </p>
-                        <span className={"d-flex flex-row"}>
-                            <img src={views} alt={"Shows the number of views"}
-                                 className={"order-3"}
-                                 style={{
-                                     marginBottom: 16,
-                                     marginLeft: 4,
-                                     marginTop: 3
-                                 }}
-                            />
-                        <p
-                            className={"order-4"}
-                            style={{
-                                marginLeft: 4,
-                                marginTop: 2
-                            }}
-                        >
-                            23
-                        </p>
-                        <p
-                            className={"order-2"}
-                            style={{
-                                color: '#1DA1F2',
-                                marginLeft: 4
-                            }}
-                        >
-                            |
-                        </p>
-                        <p
-                            className={"order-1"}
-                            style={{
-                                alignText: 'right',
-                                direction: 'rtl',
-                                marginLeft: 15
-                            }}
-                        >
-                            4 ساعت پیش
-                        </p>
-                        </span>
-
-                    </div>
-                    <div className={"Add ads button d-flex justify-content-end fixed-bottom"}
-                         style={{
-                             marginBottom: '26%',
-                             paddingRight: 15,
-                         }}
+                                marginBottom: 15,
+                                fontFamily: IranSANS
+                        }}
                     >
-                        <button
-                            className={"btn"}
-                            style={{
-                                background: '#fff',
-                                borderRadius: 15,
-                                color: '#000',
-                                width: 149,
-                                height: 60
-                            }}
-                        >
-                            افزودن آگهی
-                            <img src={plusSign} alt={"Add sign for adding ads"}
-                                 style={{
-                                     marginLeft: 10
-                                 }}
-                            />
-                        </button>
-                    </div>
-                    <div className={"Bottom nav-bar d-flex fixed-bottom justify-content-around"}
-                         style={{
-                             marginBottom: 35,
-                             marginTop:7
-                         }}
-                    >
+                        عنوان آگهی
+                    </h6>
 
-                        <img src={homePage} alt="Home page activated button"
+                    <p className={"text-end"}
+                        style={{fontSize: 10,
+                                paddingRight: 16,
+                                marginBottom: 0,
+                                fontFamily: IranSANS
+                        }}
+                    >
+                        موضوع آگهی
+                    </p>
+
+                    <p
+                        className={"text-end"}
+                        style={{
+                            fontSize: 14,
+                            paddingRight: 16,
+                            fontFamily: IranSANS,
+                            marginTop: 15,
+                            paddingBottom: 17
+                        }}
+                    >
+                        بدون محدودیت
+                    </p>
+                    <span className={"d-flex flex-row"}>
+                        <img src={views} alt={"Shows the number of views"}
                              className={"order-3"}
                              style={{
-                                 height: 30,
-                                 width: 30
+                                 marginBottom: 16,
+                                 marginLeft: 4,
+                                 marginTop: 3
                              }}
                         />
-                        <img src={chat} alt="ask page deactivated button"
-                             className={"order-5"}
-                             style={{
-                                 height: 30,
-                                 width: 30
-                             }}
-                        />
-                        <img src={profile} alt="profile page deactivated button"
-                             className={"order-1"}
-                             style={{
-                                 height: 30,
-                                 width: 30
-                             }}
-                        />
-                        <img src={wallet} alt="wallet page deactivated button"
-                             className={"order-2"}
-                             style={{
-                                 height: 30,
-                                 width: 30
-                             }}
-                        />
-                        <img src={book} alt="book page deactivated button"
-                             className={"order-4"}
-                             style={{
-                                 height: 30,
-                                 width: 30
-                             }}
-                        />
-                    </div>
-                    <div className={"fixed-bottom d-flex flex-row justify-content-around"}
-                         style={{
-                             margin: 0
-                         }}
+                    <p
+                        className={"order-4"}
+                        style={{
+                            marginLeft: 4,
+                            marginTop: 2
+                        }}
                     >
-                        <p className={"order-3"}
-                           style={{
-                               paddingRight: 5,
-                               color: '#1DA1F2'
-                           }}
-                        >
-                            آگهی ها
-                        </p>
-                        <p className={"order-2"}>
-                            کیف پول
-                        </p>
-                        <p className={"order-1"}>
-                            پروفایل
-                        </p>
-                        <p className={"order-4"}
-                           style={{
-                               paddingRight: 13
-                           }}
-                        >
-                            کتابخونه
-                        </p>
-                        <p className={"order-5"}
-                           style={{
-                               paddingRight: 12
-                           }}>
-                            بپرس
-                        </p>
-                    </div>
-                    <span className={"d-flex justify-content-center fixed-bottom"}
-                          style={{
-                              marginBottom:75
-                          }}
+                        23
+                    </p>
+                    <p
+                        className={"order-2"}
+                        style={{
+                            color: '#1DA1F2',
+                            marginLeft: 4
+                        }}
                     >
-                        <img src={blueBar} alt="The blue bar on top of the activated page"/>
+                        |
+                    </p>
+                    <p
+                        className={"order-1"}
+                        style={{
+                            alignText: 'right',
+                            direction: 'rtl',
+                            marginLeft: 15
+                        }}
+                    >
+                        4 ساعت پیش
+                    </p>
                     </span>
+
                 </div>
+                <div className={"Add ads button d-flex justify-content-end fixed-bottom"}
+                     style={{
+                         marginBottom: '26%',
+                         paddingRight: 15,
+                     }}
+                >
+                    <button
+                        className={"btn"}
+                        style={{
+                            background: '#fff',
+                            borderRadius: 15,
+                            color: '#000',
+                            width: 149,
+                            height: 60
+                        }}
+                    >
+                        افزودن آگهی
+                        <img src={plusSign} alt={"Add sign for adding ads"}
+                             style={{
+                                 marginLeft: 10
+                             }}
+                        />
+                    </button>
+                </div>
+                <div className={"Bottom nav-bar d-flex fixed-bottom justify-content-around"}
+                     style={{
+                         marginBottom: 35,
+                         marginTop:7
+                     }}
+                >
+
+                    <img src={homePage} alt="Home page activated button"
+                         className={"order-3"}
+                         style={{
+                             height: 30,
+                             width: 30
+                         }}
+                    />
+                    <img src={chat} alt="ask page deactivated button"
+                         className={"order-5"}
+                         style={{
+                             height: 30,
+                             width: 30
+                         }}
+                    />
+                    <img src={profile} alt="profile page deactivated button"
+                         className={"order-1"}
+                         style={{
+                             height: 30,
+                             width: 30
+                         }}
+                    />
+                    <img src={wallet} alt="wallet page deactivated button"
+                         className={"order-2"}
+                         style={{
+                             height: 30,
+                             width: 30
+                         }}
+                    />
+                    <img src={book} alt="book page deactivated button"
+                         className={"order-4"}
+                         style={{
+                             height: 30,
+                             width: 30
+                         }}
+                    />
+                </div>
+                <div className={"fixed-bottom d-flex flex-row justify-content-around"}
+                     style={{
+                         margin: 0
+                     }}
+                >
+                    <p className={"order-3"}
+                       style={{
+                           paddingRight: 5,
+                           color: '#1DA1F2'
+                       }}
+                    >
+                        آگهی ها
+                    </p>
+                    <p className={"order-2"}>
+                        کیف پول
+                    </p>
+                    <p className={"order-1"}>
+                        پروفایل
+                    </p>
+                    <p className={"order-4"}
+                       style={{
+                           paddingRight: 13
+                       }}
+                    >
+                        کتابخونه
+                    </p>
+                    <p className={"order-5"}
+                       style={{
+                           paddingRight: 12
+                       }}>
+                        بپرس
+                    </p>
+                </div>
+                <span className={"d-flex justify-content-center fixed-bottom"}
+                      style={{
+                          marginBottom:75
+                      }}
+                >
+                    <img src={blueBar} alt="The blue bar on top of the activated page"/>
+                </span>
             </div>
-        );
+        </div>
+    );
 }
 
 export default MainPage;
