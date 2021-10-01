9454ae921318ca96840d1874bcc3af464802e8b0
Mr. Mahmudi <l.kenway18@gmail.com>
2021-10-01 11:40:16 +0330

    SearchBar added.

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ import "./assets/fonts/fonts.css"
 import './assets/bootstrap/css/bootstrap.css'
 
 
+// import Navbar from "./components/Navbar/Navbar";
+// import SearchBar from "./components/SearchBar/SearchBar";
 import MainPage from "./components/_Pages/MainPage/MainPage";
+import NewAdvertisement from "./components/_Pages/NewAdvertisement/NewAdvertisement";
+import SearchPage from "./components/_Pages/SearchPage/SearchPage";
 
 function App() {
   return (
       <>
           <BrowserRouter>
-              <Route exact path='/' component={MainPage}/>
+              <Route exact path='/' component={SearchPage}/>
           </BrowserRouter>
       </>
   );
