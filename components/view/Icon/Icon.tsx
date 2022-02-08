b87b9c0d73a0022bf3c32131e9780b6416232fb8
Mokafela <mkd0098@gmail.com>
2022-02-08 13:35:25 +0330

    Icon View created

---
+++ components/view/Icon/Icon.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Image from "next/image";
+
+interface Props {
+    w: number,
+    h: number,
+    unit: string,
+    svg?: any,
+    img?: string,
+    alt?: string,
+    fill?: boolean
+}
+
+const Icon = ({w, h, unit, svg, img, alt, fill}: Props) => {
+
+    return (
+        img ?
+            <div
+                style={{
+                    height: h + (unit ? unit : 'px'),
+                    width: w + (unit ? unit : 'px'),
+                }}
+
+            >
+                <Image src={img} alt={alt}/>
+            </div>
+            :
+            <div className={"transition-all " + (fill ? 'icon-background' : 'bg-black')}
+                 style={{
+                     height: w + (unit ? unit : 'px'),
+                     width: w + (unit ? unit : 'px'),
+                     WebkitMaskImage: `url(${svg})`,
+                     WebkitMaskRepeat: 'no-repeat'
+                 }}>
+            </div>
+    )
+
+};
+
+export default Icon;
