c470ef63e206d989ab82f35e675271481b8dd512
Mokafela <mkd0098@gmail.com>
2022-03-30 12:31:02 +0430

    Tab Bug Fixed

--- components/view/Tab/Tab.tsx
+++ components/view/Tab/Tab.tsx
@@ -1,7 +1,6 @@
 import React, {useEffect} from 'react';
 
 
-
 interface Props {
     children?: any,
     activeIndex: number,
@@ -13,11 +12,16 @@ interface Props {
 const Tab = ({children, activeIndex, indicatorAtBottom, indicatorSizeDivider}: Props) => {
 
     const indicatorRef = React.useRef(null);
+    const container = React.useRef<HTMLDivElement>(null);
 
 
     const moveIndicator = () => {
         let children = document.getElementById('children')?.children;
         let child = children![activeIndex].getBoundingClientRect()
+        let leftOffset = 0;
+        if (container.current)
+            leftOffset = container.current.getBoundingClientRect().left
+
 
         if (indicatorRef && indicatorRef.current) {
             let indicator = (indicatorRef.current as HTMLElement)
@@ -27,7 +31,7 @@ const Tab = ({children, activeIndex, indicatorAtBottom, indicatorSizeDivider}: P
             }
             let width = child.width / divideBy;
             (indicator as HTMLElement).style.width = width + 'px';
-            (indicator as HTMLElement).style.left = ((child.left + (child.width / 2)) - (width / 2)) + 'px'
+            (indicator as HTMLElement).style.left = ((child.left - leftOffset + (child.width / 2)) - (width / 2)) + 'px'
         }
 
     }
@@ -42,7 +46,7 @@ const Tab = ({children, activeIndex, indicatorAtBottom, indicatorSizeDivider}: P
     }, [activeIndex])
 
     return (
-        <div className="w-full h-full flex flex-row justify-between">
+        <div ref={container} className="w-full h-full flex flex-row justify-between relative">
             <div ref={indicatorRef} id={'nav-indicator'}
                  className={`nav-indicator transition-all ${indicatorAtBottom ? 'bottom-0 rounded-t-md' : 'top-0 rounded-b-md'}`}/>
             <div id={'children'} className={'contents'}>
