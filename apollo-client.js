aed3f7aeb4e787e5f3890ec6a3fd098f2113ccdb
Mokafela <mkd0098@gmail.com>
2022-06-21 12:06:30 +0430

    deploy

--- apollo-client.js
+++ apollo-client.js
@@ -3,7 +3,7 @@ import {setContext} from "@apollo/client/link/context";
 import {UserToken} from "./store/user";
 
 const env = process.env.NODE_ENV
-let uri = env !== "development" ? 'https://tttapi.unimun.me/graphql' : 'https://api.unimun.me/graphql'
+let uri = env === "development" ? 'https://tttapi.unimun.me/graphql' : 'https://api.unimun.me/graphql'
 const httpLink = createHttpLink({
     uri: uri,
 });
