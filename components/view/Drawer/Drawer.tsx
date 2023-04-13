67400be9e6bc0d4c39a249e61e5ef76cd04d07fd
Mokafela <mkd0098@gmail.com>
2023-04-13 13:15:40 +0430

    prod-deploy +  login as user

--- components/view/Drawer/Drawer.tsx
+++ components/view/Drawer/Drawer.tsx
@@ -39,7 +39,7 @@ const Drawer = (props: Props) => {
     }, []);
     return (
         <div ref={drawerBack}
-             className={'fixed z-30 hide-scrollbars pointer-events-none w-full h-full bg-transparent overflow-y-scroll top-1 left-0 pointer-events-auto '}>
+             className={'fixed z-30 hide-scrollbars w-full h-full bg-transparent overflow-y-scroll top-1 left-0 pointer-events-auto '}>
             <div className={'fixed w-full h-full top-0 left-0'}>
                 {
                     props.wrap
