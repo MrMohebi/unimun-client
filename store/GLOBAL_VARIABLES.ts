bd862b6b1731313d6ad94cb711c6b8d124272ee2
Mokafela <mkd0098@gmail.com>
2023-09-26 08:17:17 +0330

    added a new admin

--- store/GLOBAL_VARIABLES.ts
+++ store/GLOBAL_VARIABLES.ts
@@ -2,7 +2,8 @@ import {makeVar} from "@apollo/client";
 
 export const UNIMUN_PROVIDERS = makeVar([
     "6257d8cc9b29fb2e9c1f3c5b",
-    "625ba548513cea117c069078"
+    "625ba548513cea117c069078",
+    '6257bb3c68ef4e6962285153'
 ])
 
 export const DOWNLOAD_HOST = makeVar('https://dl.unimun.me/')
